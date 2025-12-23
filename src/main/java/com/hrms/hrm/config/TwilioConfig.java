a9fde18f87a92a6df8141140bd5d978ece93816a
Jahnavi2909 <jahnaviboyina929@gmail.com>
2025-12-23 22:54:06 +0530

    Refactor TwilioConfig with conditional property
    Add conditional property loading for Twilio configuration.
    
    Signed-off-by: Jahnavi2909 <jahnaviboyina929@gmail.com>

--- src/main/java/com/hrms/hrm/config/TwilioConfig.java
+++ src/main/java/com/hrms/hrm/config/TwilioConfig.java
@@ -1,26 +1,9 @@
-package com.hrms.hrm.config;
-
-import com.twilio.Twilio;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-
 @Configuration
-@Getter
-@Setter
+@ConditionalOnProperty(
+  name = "twilio.enabled",
+  havingValue = "true",
+  matchIfMissing = false
+)
 public class TwilioConfig {
-
-    @Value("${twilio.accountSid}")
-    private String accountSid;
-
-    @Value("${twilio.authToken}")
-    private String authToken;
-
-    @Value("${twilio.phoneNumber}")
-    private String phoneNumber;
-
-    public void init() {
-        Twilio.init(accountSid, authToken);
-    }
+   ...
 }
